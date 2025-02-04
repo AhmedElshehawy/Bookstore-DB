3c2cf5f092e3cf18444103cef023a978e38400b0
AhmedElshehawy <ahmedelshehawy64@gmail.com>
2025-02-04 19:23:27 +0200

    Update health check route endpoint path

--- routes/health_router.py
+++ routes/health_router.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 health_router = APIRouter()
 
-@health_router.get("")
+@health_router.get("/health")
 async def health_check():
     """
     Health check endpoint for AWS Lambda
